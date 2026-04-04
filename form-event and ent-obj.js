029599529404755d647008121b6b6ba49ad71db0
kmjuli0232 <kumarijuli7018@gmail.com>
2026-04-04 14:44:50 +0530

    data types

--- form-event and ent-obj.js
+++ form-event and ent-obj.js
@@ -3,69 +3,69 @@ const usernameInput = document.querySelector("#username");
 const paragraph = document.querySelector("p");
 const form = document.querySelector("form");
 
-usernameInput.addEventListener('click', () => {
-    console.log('input clicked');
-})
+// usernameInput.addEventListener('click', () => {
+//     console.log('input clicked');
+// })
 
-usernameInput.addEventListener('dblclick', () => {
-    console.log('input double clicked');
-})
+// usernameInput.addEventListener('dblclick', () => {
+//     console.log('input double clicked');
+// })
 
-let inputValue
+// let inputValue
 
-usernameInput.addEventListener('change', (event ) => {
-console.log(event.target.value);
-paragraph.innerText = event.target.value
+// usernameInput.addEventListener('change', (event ) => {
+// console.log(event.target.value);
+// paragraph.innerText = event.target.value
 
-}
-)
-usernameInput.addEventListener('change', (e) => {
-    console.log(e);
-    inputValue = e.target.value
-    paragraph.innerText = e.target.value
-})
+// }
+// )
+// usernameInput.addEventListener('change', (e) => {
+//     console.log(e);
+//     inputValue = e.target.value
+//     paragraph.innerText = e.target.value
+// })
 
-usernameInput.addEventListener("focus", (e) => {
-  console.log(e.type);
-  console.log(e);
-  inputValue = e.target.value;
-  paragraph.innerText = e.target.value;
-});
+// usernameInput.addEventListener("focus", (e) => {
+//   console.log(e.type);
+//   console.log(e);
+//   inputValue = e.target.value;
+//   paragraph.innerText = e.target.value;
+// });
 
-usernameInput.addEventListener("blur", (e) => {
-  console.log(e.type);
-  console.log(e);
-  inputValue = e.target.value;
-  paragraph.innerText = e.target.value;
-});
+// usernameInput.addEventListener("blur", (e) => {
+//   console.log(e.type);
+//   console.log(e);
+//   inputValue = e.target.value;
+//   paragraph.innerText = e.target.value;
+// });
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const myFormData = new FormData(form)
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault()
+//     const myFormData = new FormData(form)
 
-    for (const p of myFormData.entries()) {
-        console.log(p);
-    }
-})
+//     for (const p of myFormData.entries()) {
+//         console.log(p);
+//     }
+// })
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const myFormData = new FormData(form)
-    const data = {}
-    myFormData.forEach((value,key)=>{
-        data[key]= value
-    })
-    console.log(data);
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault()
+//     const myFormData = new FormData(form)
+//     const data = {}
+//     myFormData.forEach((value,key)=>{
+//         data[key]= value
+//     })
+//     console.log(data);
 
-})
-form.addEventListener('click', (e) => {
-    e.preventDefault()
-    console.log(e);
-    // console.log('From Clicked');
-})
+// })
+// form.addEventListener('click', (e) => {
+//     e.preventDefault()
+//     console.log(e);
+//     console.log('From Clicked');
+// })
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  console.log(e);
-  console.log("From Submitted");
-});
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   console.log(e);
+//   console.log("From Submitted");
+// });
