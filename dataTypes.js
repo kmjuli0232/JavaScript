a54356c9d7db7c37a7b268b6acb0936cb5d26b35
kmjuli0232 <kumarijuli7018@gmail.com>
2026-04-05 12:20:42 +0530

    dom menupuletion

--- dataTypes.js
+++ dataTypes.js
@@ -1,30 +1,31 @@
 
-variable represent:- syntextErrore
-✅dataTypes
-✔️1. number:- represent numeric values, including integers and floating-point numbers
+// variable represent:- syntextErrore
+// ✅dataTypes
+// ✔️1. number:- represent numeric values, including integers and floating-point numbers
 
-var num =3;
-console.log(num); 
+// var num =3;
+// console.log(num); 
 
-let number =45;
-console.log(number);
+// let number =45;
+// console.log(number);
 
-✔️ 2.string:-represent refrenceError
+// ✔️ 2.string:-represent refrenceError
 
-var myName ="juli";
-console.log(myName);
+// var myName ="juli";
+// console.log(myName);
 
-✔️3. boolean:- represent logical entity with true and false
+// ✔️3. boolean:- represent logical entity with true and false
 
-var isRaning = true;
-var areYouAwesome = false;
-console.log(areYouAwesome);
+// var isRaning = true;
+// var areYouAwesome = false;
+// console.log(areYouAwesome);
 
-✔️4. undefined:-represent the absence of  a value or an uninitialized variable and show the undefined error
+// ✔️4. undefined:-represent the absence of  a value or an uninitialized variable and show the undefined error
 
-var vinod;
-console.log(vinod);
+// var vinod;
+// console.log(vinod);
 
-✔️ Null:-
+
+
 
 
